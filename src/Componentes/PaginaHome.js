a865649836c8300583b20d75edc4d8c13b9f8b75
selvinguez <42685319+selvinguez@users.noreply.github.com>
2021-12-02 21:20:24 -0600

    Rutas Creadas

---
+++ src/Componentes/PaginaHome.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default function PaginaHome() {
+  return (
+    <div style={{marginLeft:'100px',marginRight:'100px',color:'Red'}}>
+      <h1>Home</h1>
+      </div>
+  );
+}
---
+++ src/Componentes/Prim.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default function Prim() {
+  return (
+    <div style={{marginLeft:'100px',marginRight:'100px',color:'Red'}}>
+      <h1>Algoritmo de Prim</h1>
+      </div>
+  );
+}
